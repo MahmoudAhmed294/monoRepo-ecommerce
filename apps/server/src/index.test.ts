7da20e689bff49c9f8ae6c5626e766ba7484ec9b
mahmoudAhmed294 <mahmoudyoussef.dev@gmail.com>
2023-06-23 21:06:26 +0300

    feat: add public folder to server

---
+++ apps/server/src/index.test.ts
@@ -0,0 +1,5 @@
+describe('GET /api', () => {
+  it('should return a 200 status code', async () => {
+    expect(2 + 2).toBe(4);
+  });
+});
---
+++ apps/web/src/app.test.ts
@@ -0,0 +1,5 @@
+describe('test web', () => {
+  it('should return a 200 status code', async () => {
+    expect(2 + 2).toBe(4);
+  });
+});
